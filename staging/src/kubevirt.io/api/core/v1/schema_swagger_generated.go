1ea1296b59f5b4207b64faedb80d1fc7ae045660
Saravanan KR <skramaja@redhat.com>
2020-07-09 08:30:58 -0400

    Vhostuser: Added Vhostuser Interface type
    This patch adds a new interface type Vhostuser.
    It will be used to add the vhostuser interfaces to the
    VM using OvS-DPDK.
    
    Signed-off-by: Saravanan KR <skramaja@redhat.com>

--- staging/src/kubevirt.io/api/core/v1/schema_swagger_generated.go
+++ staging/src/kubevirt.io/api/core/v1/schema_swagger_generated.go
@@ -654,6 +654,10 @@ func (InterfaceMacvtap) SwaggerDoc() map[string]string {
 	return map[string]string{}
 }
 
+func (InterfaceVhostuser) SwaggerDoc() map[string]string {
+	return map[string]string{}
+}
+
 func (Port) SwaggerDoc() map[string]string {
 	return map[string]string{
 		"":         "Port repesents a port to expose from the virtual machine.\nDefault protocol TCP.\nThe port field is mandatory",
