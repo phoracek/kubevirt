dd7f8180871996c31da68ff81fbff2e422bc9e8d
Ignatious Johnson <ijohnson@juniper.net>
2021-06-18 16:46:24 -0700

    Create virsh xml elements required for vhostuser interface
    Read the vhostuser socket file name from the pod annotations
    file, which is populated via DownwardAPI/userspace CNI and use
    it in domain spec.
    
    Exported lookupInterfaceByNetwork and removed findInterfaceByNetworkName.
    Moved/exported composePodInterfaceName to network/vmispec pkg.
    These functions will be used by both virtwrap/converter and network/setup pkgs.
    
    some userspace cni can handle both vhostuser/virtio and veth interface/device, So when
    a VM can be launched with default network interface/device of type veth and additional
    network interface/device of type vhostuser/virtio. For the CNI to differentiate between
    veth device and vhostuser/virtio device, multus network annotation is specifed with
    additional CNIArgs (interface-type : virtio).
    
    Vendored following deps,
    github.com/openshift/app-netutil/lib and
    vendor/github.com/openshift/app-netutil/pkg
    
    Signed-off-by: Ignatious Johnson <ijohnson@juniper.net>

---
+++ pkg/network/vmispec/network.go
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2021 Red Hat, Inc.
+ *
+ */
+
+package vmispec
+
+import v1 "kubevirt.io/api/core/v1"
+
+func findMultusIndex(vmi *v1.VirtualMachineInstance, networkToFind *v1.Network) int {
+	idxMultus := 0
+	for _, network := range vmi.Spec.Networks {
+		if isSecondaryMultusNetwork(network) {
+			// multus pod interfaces start from 1
+			idxMultus++
+			if network.Name == networkToFind.Name {
+				return idxMultus
+			}
+		}
+	}
+	return -1
+}
+
+func isSecondaryMultusNetwork(net v1.Network) bool {
+	return net.Multus != nil && !net.Multus.Default
+}
